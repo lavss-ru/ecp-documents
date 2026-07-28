be4905ed2f43471f59ba37b3b2cbf467b4047d5d
lavss <lavss@lavss.ru>
2026-07-28 15:09:16 +0300

    feat: add Classic Editor TinyMCE button

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,3 +1,15 @@
 (function () {
 	'use strict';
+
+	tinymce.PluginManager.add('ecp_documents', function (editor) {
+
+		editor.addButton('ecp_documents', {
+			text: 'Документ с ЭП',
+
+			onclick: function () {
+				editor.windowManager.alert('Плагин ECP Documents подключен.');
+			}
+		});
+
+	});
 })();
--- src/Assets/Assets.php
+++ src/Assets/Assets.php
@@ -20,6 +20,47 @@ final class Assets {
 	 * Register WordPress hooks.
 	 */
 	public function register(): void {
-		// Asset hooks will be added here in future versions.
+		add_action( 'admin_init', array( $this, 'register_editor_hooks' ) );
+	}
+
+	/**
+	 * Register Classic Editor hooks.
+	 */
+	public function register_editor_hooks(): void {
+		add_filter(
+			'mce_external_plugins',
+			array( $this, 'register_tinymce_plugin' )
+		);
+
+		add_filter(
+			'mce_buttons',
+			array( $this, 'register_tinymce_button' )
+		);
+	}
+
+	/**
+	 * Register TinyMCE plugin.
+	 *
+	 * @param array $plugins Registered TinyMCE plugins.
+	 * @return array
+	 */
+	public function register_tinymce_plugin( array $plugins ): array {
+
+		$plugins['ecp_documents'] = ECP_DOCUMENTS_PLUGIN_URL . 'assets/js/editor.js';
+
+		return $plugins;
+	}
+
+	/**
+	 * Register TinyMCE toolbar button.
+	 *
+	 * @param array $buttons Registered toolbar buttons.
+	 * @return array
+	 */
+	public function register_tinymce_button( array $buttons ): array {
+
+		$buttons[] = 'ecp_documents';
+
+		return $buttons;
 	}
 }
