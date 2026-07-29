79d9d947a71c028fc77d3837aabe6896f5840e58
lavss <lavss@lavss.ru>
2026-07-29 15:20:51 +0300

    feat: handle PDF selection

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -9,9 +9,24 @@
 
 			this.title = '';
 
-			this.selectedPdf = null;
+			this.selectedPdf = this.createEmptySelection();
 
-			this.selectedSig = null;
+			this.selectedSig = this.createEmptySelection();
+
+			this.frames = {
+				pdf: null,
+				sig: null
+			};
+
+		}
+
+		createEmptySelection() {
+
+			return {
+				id: null,
+				filename: '',
+				url: ''
+			};
 
 		}
 
@@ -33,6 +48,143 @@
 				.find('.ecp-documents-dialog__overlay')
 				.on('click', this.closeDialog.bind(this));
 
+			this.dialog
+				.find('.ecp-select-file')
+				.on('click', this.handleSelectFile.bind(this));
+
+		}
+
+		handleSelectFile(event) {
+
+			event.preventDefault();
+
+			const type = $(event.currentTarget).data('type');
+
+			this.openMedia(type);
+
+		}
+
+		openMedia(type) {
+
+			if (!this.frames[type]) {
+
+				this.frames[type] = this.createMediaFrame(type);
+
+			}
+
+			this.frames[type].open();
+
+		}
+
+		createMediaFrame(type) {
+
+			const frame = wp.media(
+				this.getMediaConfig(type)
+			);
+
+			frame.on(
+				'select',
+				() => this.handleMediaSelect(type, frame)
+			);
+
+			return frame;
+
+		}
+
+		getMediaConfig(type) {
+
+			const configs = {
+
+				pdf: {
+
+					title: 'Выберите PDF-документ',
+
+					button: {
+						text: 'Выбрать'
+					},
+
+					library: {
+						type: 'application/pdf'
+					},
+
+					multiple: false
+
+				},
+
+				sig: {
+
+					title: 'Выберите SIG-файл',
+
+					button: {
+						text: 'Выбрать'
+					},
+
+					library: {},
+
+					multiple: false
+
+				}
+
+			};
+
+			return configs[type];
+
+		}
+
+		handleMediaSelect(type, frame) {
+
+			const attachment = frame
+				.state()
+				.get('selection')
+				.first()
+				.toJSON();
+
+			this.setSelection(type, attachment);
+
+		}
+
+		setSelection(type, attachment) {
+
+			const selection = {
+				id: attachment.id,
+				filename: attachment.filename,
+				url: attachment.url
+			};
+
+			if (type === 'pdf') {
+
+				this.selectedPdf = selection;
+
+			} else {
+
+				this.selectedSig = selection;
+
+			}
+
+			this.updateFileUI(type);
+
+		}
+
+		updateFileUI(type) {
+
+			const selection = type === 'pdf'
+				? this.selectedPdf
+				: this.selectedSig;
+
+			const field = this.dialog.find(
+				'.ecp-field[data-type="' + type + '"]'
+			);
+
+			field
+				.find('.ecp-file-placeholder')
+				.prop('hidden', true);
+
+			field
+				.find('.ecp-file-link')
+				.text(selection.filename)
+				.attr('href', selection.url)
+				.prop('hidden', false);
+
 		}
 
 		openDialog() {
@@ -51,9 +203,9 @@
 
 			this.title = '';
 
-			this.selectedPdf = null;
+			this.selectedPdf = this.createEmptySelection();
 
-			this.selectedSig = null;
+			this.selectedSig = this.createEmptySelection();
 
 		}
 
