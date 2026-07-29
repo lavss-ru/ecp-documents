ec80261781bb0616649af6f0b1a088da26e44495
lavss <lavss@lavss.ru>
2026-07-29 13:31:33 +0300

    refactor: prepare admin.js architecture

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -3,12 +3,42 @@
 
 	window.ECPDocuments = {
 
+		dialog: null,
+
+		title: '',
+
+		selectedPdf: null,
+
+		selectedSig: null,
+
+		init: function () {
+			// Инициализация плагина.
+		},
+
 		openDialog: function () {
 
 			alert('admin.js подключен');
 
+		},
+
+		closeDialog: function () {
+			// Здесь позже будет закрытие модального окна.
+		},
+
+		reset: function () {
+
+			this.title = '';
+			this.selectedPdf = null;
+			this.selectedSig = null;
+
 		}
 
 	};
+
+	$(function () {
+
+		window.ECPDocuments.init();
+
+	});
 
 })(jQuery);
