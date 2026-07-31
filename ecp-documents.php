76636603afbbd24e598ccbee2aa31607f2d7cace
lavss <lavss@lavss.ru>
2026-07-31 13:54:41 +0300

    chore: add plugin directory constant

--- ecp-documents.php
+++ ecp-documents.php
@@ -21,6 +21,7 @@ declare(strict_types=1);
 defined( 'ABSPATH' ) || exit;
 
 define( 'ECP_DOCUMENTS_PLUGIN_FILE', __FILE__ );
+define( 'ECP_DOCUMENTS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'ECP_DOCUMENTS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 require_once __DIR__ . '/vendor/autoload.php';
